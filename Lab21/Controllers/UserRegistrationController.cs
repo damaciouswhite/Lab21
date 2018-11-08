dc87e342f3a66d32e9daa71b8128dcd05c8f6ebb
Damacious <damaciouswhite@yahoo.com>
2018-11-08 10:16:28 -0500

    Lab 21

---
+++ Lab21/Controllers/UserRegistrationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab21.Views.UserRegistration
+{
+    public class UserRegistrationController : Controller
+    {
+        // GET: UserRegistration
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
